afd55f51b17fa82a60a383fca6ed10f380977eeb
SaySidMaster <sidibesaydil@gmail.com>
2026-05-07 22:09:21 +0000

    Setting sessionId routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\MemeController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/session',     [MemeController::class, 'sessionInfo']);
 Route::get('/memes',       [MemeController::class, 'index']);   // Galerie React
 Route::post('/generate',   [MemeController::class, 'store']);   // Création
 Route::delete('/deleteMeme', [MemeController::class, 'destroy']); // Suppression (dev)
